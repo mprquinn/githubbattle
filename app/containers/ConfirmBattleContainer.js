a92a59944bb6460d3829abec06255b964102b732
Mike Tomothy Q <mike@simplesimple.ca>
2016-03-22 15:16:06 -0600

    begin battle container

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,26 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+
+var ConfirmBattleContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    return {
+      isLoading: true,
+      playerInfo: []
+    }
+  },
+  componentDidMount: function () {
+    var query = this.props.location.query;
+    console.log('QUERY', query);
+    // var query - this.props;
+  },
+  render: function () {
+    return (
+      <ConfirmBattle />
+    )
+  }
+});
+
+module.exports = ConfirmBattleContainer;
